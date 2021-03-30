7e7a2545bc5be6c723768a553a2d82e74cfebe3e
Kevin Muchene <muchenekevin@gmail.com>
2021-03-30 14:08:04 +0200

    Add functionalities in Body.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import Body from "./components/Body";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, CardColumns, CardDeck } from "react-bootstrap";
 
 function App() {
   return (
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import DisplayRecipe from "./DisplayRecipe";
 
 const APP_ID = "742d5385";
 const APP_KEY = "6f2f4156d908b7af3d69c91a2bb763cd";
 
 const Body = () => {
-  useEffect(() => {}); //end useEffect hook
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+  useEffect(() => {
+    getRecipes();
+  }, [query]); //end useEffect hook
 
   const getRecipes = async () => {
     const response = await fetch(
-      `htttps://api.edamam.com/search?&q=&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
+
+    setRecipes(data.hits);
   }; //end getRecipes function
 
-  const updateSearch = (e) => {}; //end updateSearch
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  }; //end updateSearch
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={getSearch}>
         <input
+          value={search}
           type="text"
           placeholder="Search..."
           onChange={updateSearch}
         ></input>
         <button>Search</button>
       </form>
+      <div className="cards">
+        {recipes.map((recipe) => (
+          <DisplayRecipe
+            title={recipe.recipe.label}
+            image={recipe.recipe.image}
+            ingredientLines={recipe.recipe.ingredientLines}
+            calories={recipe.recipe.calories}
+          />
+        ))}
+      </div>
     </div>
   );
 }; //end body component
