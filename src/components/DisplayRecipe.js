d15143e88137a8a5e66460ed79b278a6a43f93c1
Kevin Muchene <muchenekevin@gmail.com>
2021-03-30 14:06:07 +0200

    Used Bootstrap Cards to render recipes

--- src/components/DisplayRecipe.js
+++ src/components/DisplayRecipe.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { Card, Accordion, Button, CardDeck } from "react-bootstrap";
 
-const DisplayRecipe = () => {
-  return <div></div>;
+const DisplayRecipe = ({ title, image, ingredientLines, calories }) => {
+  return (
+    <div>
+      <CardDeck>
+        <Card className="card">
+          <Card.Img variant="top" src={image} />
+          <Card.Body>
+            <Card.Title className="title">{title}</Card.Title>
+            <h6>Calories: {Math.floor(calories)}</h6>
+            <Card.Text>
+              <Accordion>
+                <Card>
+                  <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                      Check recipe
+                    </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                      <ol>
+                        {ingredientLines.map((ingredient) => (
+                          <li>{ingredient}</li>
+                        ))}
+                      </ol>
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </CardDeck>
+    </div>
+  );
 }; //end DisplayRecipe
 
 export default DisplayRecipe;
