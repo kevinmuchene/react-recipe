e7da005fa89ef9b4f76f728b944a90533189b318
Kevin Muchene <muchenekevin@gmail.com>
2021-03-31 19:39:44 +0200

    Header,js contains the Apps name and details

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Recipe App</h1>
+      <p>
+        <b>
+          {" "}
+          <i>This site uses Edamam's Recipe Search API to intergrate recipes</i>
+        </b>
+      </p>
+    </div>
+  );
+}; //end carousel component
+
+export default Header;
